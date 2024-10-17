428a933d9a2e220b467ff10b3ce2e701504e41ca
FauZa <160544435+FauZaPespi@users.noreply.github.com>
2024-10-17 11:33:09 +0200

    Upload des files

---
+++ P_UTF-8_HEX/Frm_StringHex.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace P_UTF_8_HEX
+{
+    partial class Frm_StringHex
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_StringHex));
+            this.tbxString = new System.Windows.Forms.TextBox();
+            this.tbxHex = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxString
+            // 
+            this.tbxString.Location = new System.Drawing.Point(12, 12);
+            this.tbxString.Multiline = true;
+            this.tbxString.Name = "tbxString";
+            this.tbxString.Size = new System.Drawing.Size(776, 96);
+            this.tbxString.TabIndex = 0;
+            this.tbxString.TextChanged += new System.EventHandler(this.tbxString_TextChanged);
+            // 
+            // tbxHex
+            // 
+            this.tbxHex.Location = new System.Drawing.Point(12, 224);
+            this.tbxHex.Multiline = true;
+            this.tbxHex.Name = "tbxHex";
+            this.tbxHex.ReadOnly = true;
+            this.tbxHex.Size = new System.Drawing.Size(776, 96);
+            this.tbxHex.TabIndex = 1;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(271, 145);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(259, 42);
+            this.lblInfo.TabIndex = 2;
+            this.lblInfo.Text = "Convertir Hex";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Frm_StringHex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 333);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.tbxHex);
+            this.Controls.Add(this.tbxString);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 372);
+            this.MinimumSize = new System.Drawing.Size(816, 372);
+            this.Name = "Frm_StringHex";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Convertiseur de text en hexadécimal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxString;
+        private System.Windows.Forms.TextBox tbxHex;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ P_UTF-8_HEX/Frm_StringHex.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace P_UTF_8_HEX
+{
+    /// <summary>
+    /// Auteur: FauZaPespi
+    /// Date: 17.10.2024
+    /// Détails: Convertir une chaine de charactère en hexadécimal
+    /// </summary>
+    public partial class Frm_StringHex : Form
+    {
+        /// <summary>
+        /// Constructeur de class
+        /// </summary>
+        public Frm_StringHex()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Méthode pour tbxString quand son texte est modifié
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbxString_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                tbxHex.Text = ConvertToHex(tbxString.Text);
+            }
+            catch
+            {
+                tbxString.Text = "";
+                tbxHex.Text = "";
+                MessageBox.Show("Le programme n'arrive pas a convertir...", "StringHex.exe");
+            }
+        }
+        /// <summary>
+        /// Cette methode prend en parametre Chaine qui est un string comme "Bonjour" et va le convertir en hexadécimal et retourné ce résultat
+        /// </summary>
+        /// <param name="Chaine">Chaine entré</param>
+        /// <returns>Valeur hexadécimal de Chaine</returns>
+        private string ConvertToHex(string Chaine)
+        {
+            byte[] ba = Encoding.Default.GetBytes(Chaine);
+            var hexString = BitConverter.ToString(ba);
+            return hexString.Replace("-", "|");
+        }
+    }
+}
